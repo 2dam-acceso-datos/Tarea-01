4acdf6d0f1aa034768b5cb7fc954d3290a0d9564
jerem <jeremiasacortes@gmail.com>
2025-09-29 01:27:36 +0200

    Added: File header

--- Tarea-01/src/Main.java
+++ Tarea-01/src/Main.java
@@ -1,11 +1,18 @@
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         ReservationAll reservePt1 = new ReservationAll("reservas.txt");
+        File file = reservePt1.createFile();  // guardar referencia al archivo creado
 
-        reservePt1.createFile();
-        
         System.out.println(reservePt1);
+
+        ReservationAll.writeHeaders(
+                file,
+                ReservationFields.SEAT_NUMBER,
+                ReservationFields.PASSENGER_NAME,
+                ReservationFields.CLASS
+        );
     }
 }
