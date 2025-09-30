6cf40d279ede371bac8729a63a0f1f903f0717ff
jerem <jeremiasacortes@gmail.com>
2025-09-30 23:06:41 +0200

    Added: Fill records

--- Tarea-01/src/Main.java
+++ Tarea-01/src/Main.java
@@ -5,17 +5,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ReservationAll reservePt1 = new ReservationAll("reservas.txt");
         File file1 = reservePt1.createFile();  // guardar referencia al archivo creado
-
         reservePt1.writeHeaders(
                 file1,
                 ReservationFields.SEAT_NUMBER,
                 ReservationFields.PASSENGER_NAME,
                 ReservationFields.CLASS
         );
-
-        reservePt1.pickHowManyRegisters();
+//        reservePt1.pickHowManyRegisters();
 //        reservePt1.logguer();
-        reservePt1.logguer(ReservationClass.BUSINESS);
+//        reservePt1.logguer(ReservationClass.BUSINESS);
 
         ReservationAll reservePt2 = new ReservationAll(true, "reservas_maestro.txt");
         File file2 = reservePt2.createFile();  // guardar referencia al archivo creado
@@ -27,5 +25,6 @@ public class Main {
                 ReservationFields.CLASS,
                 ReservationFields.DESTINATION
         );
+        reservePt2.pickHowManyRegisters();
     }
 }
