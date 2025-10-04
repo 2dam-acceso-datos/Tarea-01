a61603af0e68d60e258dcc78ae329e2f6418780b
Irene Fontcuberta León <159274073+irenefont@users.noreply.github.com>
2025-10-04 21:02:42 +0200

    Added: If the records are not complete, logs are created and the number of valid records is displayed.

--- Tarea-01/src/Reservation/Main.java
+++ Tarea-01/src/Reservation/Main.java
@@ -17,20 +17,20 @@ public class Main {
 //        reservePt1.pickHowManyRegisters();
 //        reservePt1.logguer();
 //
-//        ReservationAll reservePt2 = new ReservationAll(true, "reservas_maestro.txt");
-//        File file2 = reservePt2.createFile();  // guardar referencia al archivo creado
-//
-//        reservePt2.writeHeaders(
-//                file2,
-//                ReservationFields.SEAT_NUMBER,
-//                ReservationFields.PASSENGER_NAME,
-//                ReservationFields.CLASS,
-//                ReservationFields.DESTINATION
-//        );
-//        reservePt2.pickHowManyRegisters();
-//        reservePt2.createandFillFileByDestination(ReservationFields.DESTINATION);
-//        reservePt2.logguer(Reservation.ReservationClass.BUSINESS);
-//        ReservationAll.showReservationsByCountry(Destinations.TOKIO);
+        ReservationAll reservePt2 = new ReservationAll(true, "reservas_maestro.txt");
+        File file2 = reservePt2.createFile();  // guardar referencia al archivo creado
+
+        reservePt2.writeHeaders(
+                file2,
+                ReservationFields.SEAT_NUMBER,
+                ReservationFields.PASSENGER_NAME,
+                ReservationFields.CLASS,
+                ReservationFields.DESTINATION
+        );
+        reservePt2.pickHowManyRegisters();
+        reservePt2.createandFillFileByDestination(ReservationFields.DESTINATION);
+        reservePt2.logguer(Reservation.ReservationClass.BUSINESS);
+        ReservationAll.showReservationsByCountry(Destinations.TOKIO);
 
         ReservationAll reservePt3 = new ReservationAll(false, "reservas_maestro_con_errores.txt");
         File file3 = reservePt3.createFile();  // guardar referencia al archivo creado
